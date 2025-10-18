482eecacb52517f2d5b4c1ef066162bdc26e81ac
LB094 <142070410+LB094@users.noreply.github.com>
2025-10-18 19:28:46 +0100

    joltik

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ fetch('rsv10pt5.json')
     cards.forEach(card => {
       const div = document.createElement('div');
       div.classList.add('card');
+      //Does the text for the code   Line 12 and 13 if it doesn't have a HP/Type THEN display supertype/subtype
       div.innerHTML = `
         <img src="${card.images?.small}" alt="${card.name}">
         <h2>${card.name}</h2>
-        <p><strong>${card.hp ? 'HP' : 'Supertype'}:</strong> ${card.hp || card.supertype}</p> 
+        <p><strong>${card.hp ? 'HP' : 'Type'}:</strong> ${card.hp || card.supertype}</p> 
         <p><strong>${card.types ? 'Type' : 'Subtype'}:</strong> ${card.types ? card.types.join(', ') : card.subtypes?.join(', ')}</p>
         <p><strong>Rarity:</strong> ${card.rarity}</p> 
       `;
