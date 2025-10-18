6778da1f954da2f2f8c5c5f9aafb32208848db08
LB094 <142070410+LB094@users.noreply.github.com>
2025-10-18 14:32:39 +0100

    test

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ fetch('rsv10pt5.json')
         <img src="${card.images?.small}" alt="${card.name}">
         <h2>${card.name}</h2>
         <p><strong>${card.hp ? 'HP' : 'Supertype'}:</strong> ${card.hp || card.supertype}</p> 
-        <p><strong>Type:</strong> ${card.types ? '' : 'Subtypes'}:</strong> ${card.types || card.subtype}</p>
+        <p><strong>${card.types ? 'Type' : 'Subtype'}:</strong> ${card.types ? card.types.join(', ') : card.subtypes?.join(', ')}</p>
         <p><strong>Rarity:</strong> ${card.rarity}</p>
       `;
       container.appendChild(div);
